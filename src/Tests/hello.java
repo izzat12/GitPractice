a45883abe5eb5c6e710ea593a37384fe1edffa45
izzat12 <Izzatpatidinov@gmail.com>
2020-04-23 15:42:41 -0400

    Tests

--- src/Tests/hello.java
+++ src/Tests/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("hello Tests");
+        System.out.println("hello");
     }
 }
